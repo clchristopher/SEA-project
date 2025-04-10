7325461e1f5a38bba1e6fc078c39878f0d714170
clchristopher <clchristopherliu@gmail.com>
2025-04-10 15:36:02 -0700

    change to board games and add bullet points

--- scripts.js
+++ scripts.js
@@ -23,19 +23,49 @@
  *
  */
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+// const FRESH_PRINCE_URL =
+//   "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
+// const CURB_POSTER_URL =
+//   "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
+// const EAST_LOS_HIGH_POSTER_URL =
+//   "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
 // This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
+
+// Board Game Catalog - Array of Objects
+let gameDirectory = [
+  {
+    id: 1,
+    title: "Catan",
+    imageURL: "https://cf.geekdo-images.com/W3Bsga_uLP9kO91gZ7H8yw__imagepage/img/M_3Vg1j2HlNgkv7PL2xl2BJE2bw=/fit-in/900x600/filters:no_upscale():strip_icc()/pic2419375.jpg",
+    releaseYear: 1995,
+    players: "3-4",
+    playTime: "60-120 minutes",
+    description: "In this game, players try to establish settlements on the island of Catan by building roads, cities, and settlements. Players collect and trade resources to build and expand their civilization."
+  },
+  {
+    id: 2,
+    title: "Tapple",
+    imageURL: "https://cf.geekdo-images.com/ug2wHUhS3E-LA_lmLQYyNQ__imagepage/img/EOvL07ZTPgjIzy0O6-sOTHPe_sk=/fit-in/900x600/filters:no_upscale():strip_icc()/pic6650117.jpg",
+    releaseYear: 1995,
+    players: "3-4",
+    playTime: "60-120 minutes",
+    description: "Players shout answers to trivia categories and eliminate letters as they go. The game is fast-paced and requires quick thinking and a good vocabulary."
+  },
+  {
+    id: 3,
+    title: "Codenames",
+    imageURL: "https://cf.geekdo-images.com/F_KDEu0GjdClml8N7c8Imw__imagepage/img/rc_Do8f5v41nWEGcwHE1eKAkIfI=/fit-in/900x600/filters:no_upscale():strip_icc()/pic2582929.jpg",
+    releaseYear: 2015,
+    players: "2-8",
+    playTime: "15 minutes",
+    description: "Give your team clever one-word clues to help them spot their agents in the field. But be careful! The other team is trying to do the same thing, and you don't want to help them out."
+  },
+  
 ];
+
+// Keep a reference to the original data so we can reset filters
+let displayedItems = [...gameDirectory];
 // Your final submission should have much more data than this, and
 // you should use more than just an array of strings to store it all.
 
@@ -45,27 +75,19 @@ function showCards() {
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
+  for (let i = 0; i < gameDirectory.length; i++) {
+    let game = gameDirectory[i];
+    let bulletPoints = [game.releaseYear, game.players, game.playTime, game.description];
     // This part of the code doesn't scale very well! After you add your
     // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard, game.title, game.imageURL, bulletPoints); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+function editCardContent(card, newTitle, newImageURL, newInfo) {
   card.style.display = "block";
 
   const cardHeader = card.querySelector("h2");
@@ -74,6 +96,15 @@ function editCardContent(card, newTitle, newImageURL) {
   const cardImage = card.querySelector("img");
   cardImage.src = newImageURL;
   cardImage.alt = newTitle + " Poster";
+  for (let i = 0; i < newInfo.length; i++) {
+    const list = card.querySelector("ul");
+    list.innerHTML = ""; // Clear old list items
+    newInfo.forEach(info => {
+      const li = document.createElement("li");
+      li.textContent = info;
+      list.appendChild(li);
+    });
+  }
 
   // You can use console.log to help you debug!
   // View the output by right clicking on your website,
